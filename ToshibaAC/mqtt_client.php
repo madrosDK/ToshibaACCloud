820071e262af5fdc8dc9e5bda02b86c8ca8d898b
madros <danielkoglek@gmx.at>
2026-04-29 12:58:09 +0200

    Add encode helpers

--- ToshibaAC/mqtt_client.php
+++ ToshibaAC/mqtt_client.php
@@ -2,4 +2,22 @@
 
 class ToshibaACMQTTClient
 {
+    private function encodeString(string $value): string
+    {
+        return pack('n', strlen($value)) . $value;
+    }
+
+    private function encodeLength(int $length): string
+    {
+        $encoded = '';
+        do {
+            $digit = $length % 128;
+            $length = intdiv($length, 128);
+            if ($length > 0) {
+                $digit |= 0x80;
+            }
+            $encoded .= chr($digit);
+        } while ($length > 0);
+        return $encoded;
+    }
 }
